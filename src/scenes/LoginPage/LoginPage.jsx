cb593c05764487ac55fa15c01f529235ba225f71
ApuEmdad <emdadul.apu@savethechildren.org>
2023-03-04 00:42:12 +0600

    Navbar for both wide and mobile screen is completed

--- src/scenes/LoginPage/LoginPage.jsx
+++ src/scenes/LoginPage/LoginPage.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 
 const LoginPage = () => {
   return (
     <div>
+      <Navbar />
       <h2>This is Loginpage</h2>
     </div>
   );
