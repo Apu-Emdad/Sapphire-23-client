33d08bf8b553f15c4a84cc60984fa43d6d13ab57
ApuEmdad <emdadul.apu@savethechildren.org>
2023-07-02 01:00:39 +0600

    before dropzone

--- src/components/LoginForm/AuthForm.jsx
+++ src/components/LoginForm/AuthForm.jsx
@@ -20,8 +20,8 @@ import { loginScheama } from "../Schema/Schema";
 import { Formik } from "formik";
 
 const LoginForm = () => {
-  const [pageType, setPageType] = useState("login");
-  const { pallete } = useTheme();
+  const [pageType, setPageType] = useState("register");
+  const { palette } = useTheme();
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const isWideScreen = useMediaQuery("(min-width:600px)");
@@ -107,7 +107,81 @@ const LoginForm = () => {
         resetForm,
       }) => (
         <form action="" onSubmit={handleSubmit}>
-          <Box display="grid" gap="30px"></Box>
+          <Box
+            display="grid"
+            gap="30px"
+            gridTemplateColumns="repeat(4, minmax(0,1fr))"
+            sx={{
+              "& > div": { gridColumn: isWideScreen ? undefined : "span 4" },
+            }}
+          >
+            {isRegister && (
+              <>
+                {/* ++++ First Name ++++ */}
+                <TextField
+                  label="First Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.firstName}
+                  name="firstName"
+                  error={
+                    Boolean(touched.firstName) && Boolean(errors.firstName)
+                  }
+                  helperText={touched.firstName && errors.firstName}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                {/* ---- First Name ----  */}
+
+                {/* ++++ Last Name ++++ */}
+                <TextField
+                  label="last Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.lastName}
+                  name="lastName"
+                  error={Boolean(touched.lastName) && Boolean(errors.lastName)}
+                  helperText={touched.lastName && errors.lastName}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                {/* ---- Last Name ----  */}
+
+                {/* ++++ Location ++++ */}
+                <TextField
+                  label="location"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.location}
+                  name="location"
+                  error={Boolean(touched.location) && Boolean(errors.location)}
+                  helperText={touched.location && errors.location}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                {/* ---- Location ----  */}
+
+                {/* ++++ occupation ++++ */}
+                <TextField
+                  label="occupation"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.occupation}
+                  name="occupation"
+                  error={
+                    Boolean(touched.occupation) && Boolean(errors.occupation)
+                  }
+                  helperText={touched.occupation && errors.occupation}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                {/* ---- occupation ----  */}
+
+                <Box
+                  gridColumn="span 4"
+                  border={`1px solid ${palette.neutral.medium}`}
+                  borderRadius="5px"
+                  p="1rem"
+                ></Box>
+              </>
+            )}
+          </Box>
         </form>
       )}
     </Formik>
